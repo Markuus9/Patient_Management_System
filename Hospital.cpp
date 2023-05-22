3a051c83bb7f8438b6af88697913576ceb1a8ead
Markus <55411561+Markuus9@users.noreply.github.com>
2023-05-22 20:11:06 +0200

    Update Hospital.cpp

--- Hospital.cpp
+++ Hospital.cpp
@@ -18,13 +18,31 @@ void Hospital::baixa_pacient(){
     if(par.first) {
         Pacients.remove(par.second);
         LlistaEspera.remove(par.second);
+    } else {
+        cerr << " Error" << endl;
     }
-    else cerr << " Error" << endl;
 }
 
+void Hospital::alta_doctor(){
+    Doctor D;
+    cin >> D;
+    if(not Doctors.find(D).first){
+        Doctors.insert(D);
+    } else {
+        cerr << " error" << endl;
+    }
+}
 
 void Hospital::llista_espera(){
     cout << LlistaEspera;
+}
+
+void Hospital::tractar_seguent_pacient(){
+    if(LlistaEspera.empty()==true){
+        cerr << " error" << endl;
+    } else {
+        LlistaEspera.pop();
+    }
 }
 
 void Hospital::modificar_estat_pacient() {
@@ -42,4 +60,40 @@ void Hospital::modificar_estat_pacient() {
     par.second.actualitzaEstat(estat);
     Pacients.setValue(p,par.second);
     LlistaEspera.push(par.second);
+}
+
+void Hospital::programar_visita(){
+    string nom;
+    Doctor doctor;
+    Data data;
+    cin >> nom >> doctor >> data;
+    Pacient pacient(nom);
+    if (not Doctors.find(doctor).first or not Pacients.find(pacient).first) {
+        pair<Pacient,Data> visita(pacient,data);
+        Doctor copia = Doctors.find(doctor).second;
+        doctor.crear_visita(visita);
+        Doctors.setValue(copia, doctor);
+    } else {
+        cerr << " error" << endl;
+    }
+}
+
+void Hospital::cancellar_visita(){
+    string nom;
+    Doctor doctor;
+    Data data;
+    cin >> nom >> doctor >> data;
+    Pacient pacient(nom);
+    pair<Pacient,Data> visita(pacient,data);
+    if (Doctors.find(doctor).first and Pacients.find(pacient).first and data==Doctors.find(doctor).second.getData(visita)) {
+       Doctor copia = Doctors.find(doctor).second;
+       doctor.eliminar_visita(visita);
+       Doctors.setValue(copia, doctor);
+    } else {
+        cerr << " error" << endl;
+    }  
+}
+
+void Hospital::mostrar_programacio_visites(){
+   Doctors.print();
 }
